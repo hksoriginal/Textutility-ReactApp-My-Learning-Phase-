3433fa37681a8428ef33a0b40632c0cbfead34ee
Harshit Kumar <44989568+hksoriginal@users.noreply.github.com>
2021-12-30 13:37:29 +0530

    Updates

---
+++ precache-manifest.481db8177e4cab4635f23e286dea2b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "715ca2139e2eae37c380",
+    "url": "/Textutility-ReactApp-My-Learning-Phase-/static/css/main.0ccdb9fa.chunk.css"
+  },
+  {
+    "revision": "715ca2139e2eae37c380",
+    "url": "/Textutility-ReactApp-My-Learning-Phase-/static/js/main.715ca213.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/Textutility-ReactApp-My-Learning-Phase-/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/Textutility-ReactApp-My-Learning-Phase-/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "1d66dfe949c8dea92f4f",
+    "url": "/Textutility-ReactApp-My-Learning-Phase-/static/js/runtime~main.1d66dfe9.js"
+  },
+  {
+    "revision": "2234f582ba3ea1d37de694b8c3ce10f3",
+    "url": "/Textutility-ReactApp-My-Learning-Phase-/index.html"
+  }
+];
